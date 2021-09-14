b25832fad55829ca9dc4bf3f1502e605fbb1f009
Mark O Grady <mark.o'grady@broadbean.com>
2021-09-14 01:26:19 +0100

    plugin test placeeholders

---
+++ __tests__/store.ts
@@ -0,0 +1,9 @@
+describe("testing with ioredis package", () => {
+
+  test("a queue can be created", async () => {
+
+    expect(1).toBe(1);
+
+  });
+
+});
---
+++ jest.config.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+  maxWorkers: '50%',
+  testPathIgnorePatterns: ['<rootDir>/__tests__/utils'],
+  transform: {
+    '^.+\\.ts?$': 'ts-jest',
+  },
+};
